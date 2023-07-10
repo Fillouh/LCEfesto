805ec813c204a1015a553a272567b97d2aaf9118
Filippo Leonelli <300717@studenti.unimore.it>
2023-07-10 13:09:20 +0200

    stats update

--- src/main/java/com/lcefesto/ops/Statistics.java
+++ src/main/java/com/lcefesto/ops/Statistics.java
@@ -2,40 +2,40 @@ package com.lcefesto.ops;
 
 public class Statistics {
     //COMBINATORICS
-    public static double factorial(double n){
-        if(n<0){
+    public static double factorial(double number){
+        if(number<0){
             throw new IllegalArgumentException("The parameter n must be a positive!");
         }
-        if(n==0){
+        if(number==0){
             return 1;
         }
-        double number=n;
-        for(double i=1;i<n;++i){
+        double num=number;
+        for(double i=1;i<number;++i){
             number*=i;
         }
         return number;
     }
 
-    public static double semiFactorial(double n){
-        if(n<0){
+    public static double semiFactorial(double number){
+        if(number<0){
             throw new IllegalArgumentException("The parameter n must be a positive integer!");
         }
-        if(n==0||n==1){
+        if(number==0||number==1){
             return 1;
         }
         double ris = 1;
-        for (double i = n; i > 1; i-=2) {
+        for (double i = number; i > 1; i-=2) {
             ris *= i;
         }
         return ris;
     }
 
-    public static double arrangementsWithRepetition(double n,double k){
-        return Math.pow(n,k);
+    public static double arrangementsWithRepetition(double nElements,double kPositions){
+        return Math.pow(nElements,kPositions);
     }
 
-    public static double arrangementsWithoutRepetition(double n, double k){
-        return factorial(n)/factorial(n-k);
+    public static double arrangementsWithoutRepetition(double nElements, double kPositions){
+        return factorial(nElements)/factorial(nElements-kPositions);
     }
 
     public static double binomialCoefficient(double n,double k){
